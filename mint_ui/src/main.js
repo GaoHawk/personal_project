732692a3d727c1b7f336c2d000d05862cbf28105
LS--20160803ISH\Administrator <897622361@qq.com>
2017-04-07 14:29:11 +0800

    mint_ui part3

---
+++ mint_ui/src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './route'
+import { install as Mint } from './index'
+
+
+Vue.use(Mint);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  base: __dirname,
+  routes
+});
+
+new Vue({ // eslint-disable-line
+  el: '#app',
+  render: h => h(App),
+  router
+});
+
+let indexScrollTop = 0;
+router.beforeEach((route, redirect, next) => {
+  if (route.path !== '/') {
+    indexScrollTop = document.body.scrollTop;
+  }
+  document.title = route.meta.title || document.title;
+  next();
+});
+
+router.afterEach(route => {
+  if (route.path !== '/') {
+    document.body.scrollTop = 0;
+  } else {
+    Vue.nextTick(() => {
+      document.body.scrollTop = indexScrollTop;
+    });
+  }
+});
