7ac852b1908718e578e679c760bf3cd276939965
LS--20160803ISH\Administrator <897622361@qq.com>
2017-04-07 14:48:05 +0800

    mint v2

---
+++ mint_with_vue2.0/src/component/bar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="template">
+     <child></child>
+     Bar
+     </div>
+</template>
+<style>
+   .template{
+      background: black;
+   }
+</style>
+<script>
+import Header from './header.vue'
+export default {
+   data(){
+      return {
+         name:'Bar',
+
+      }
+   },
+   components:{
+       'child':Header
+   }
+}
+</script>
